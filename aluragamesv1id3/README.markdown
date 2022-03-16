e2e0849e6f5cb9c950f3c469364ae6cbb3614168
Leonardo de Oliveira Gonçalves <37309720+lrd003@users.noreply.github.com>
2022-03-16 09:51:07 -0300

    att

---
+++ aluragamesv1id3/README.markdown
@@ -0,0 +1,5 @@
+# AluraGamesv1- ID3
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/lrd003/pen/WNdbEGL](https://codepen.io/lrd003/pen/WNdbEGL).
+
+
---
+++ aluragamesv1id3/dist/script.js
@@ -0,0 +1,12 @@
+var listaFilmes = [
+  "https://gametimes.com.br/wp-content/uploads/2020/07/valorant-pergunte-3-scaled.jpg",
+  "https://www.mktesportivo.com/wp-content/uploads/2022/03/csgo.jpeg",
+  "https://www.blogs.unicamp.br/ciencianerd/wp-content/uploads/sites/113/2021/07/C_C-POP_0fe20042_0bb8_4781_82f4_7130f928b021.jpg",
+  "https://s2.glbimg.com/7qX20CsOFnt8HEDNgyVGZHs4nkE=/0x0:800x470/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/2/T/qeeXfESYmT9X3L4Yw7DA/smilegate.jpg",
+  "https://alternativanerd.com.br/wp-content/uploads/2021/01/AN_Black-Desert-Nova.jpg",
+  "https://s2.glbimg.com/fxI0yK-cdnMNI2FrieyuHcdCtkE=/0x0:984x554/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2021/6/Q/Fq6AMUTJKxZA7SVgIJBA/league-of-legends.jpg"
+];
+
+for (var indice = 0; indice < listaFilmes.length; indice++) {
+  document.write("<img src=" + listaFilmes[indice] + ">");
+}
